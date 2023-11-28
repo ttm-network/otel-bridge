88b755163ce5bbbf37603686d06dc3d65c383bc4
thenotsoft <thenotsoft@gmail.com>
2023-11-28 16:27:11 +0200

    wip

--- config/di.php
+++ config/di.php
@@ -2,4 +2,43 @@
 
 declare(strict_types=1);
 
-return [];
+use OpenTelemetry\API\Trace\Propagation\TraceContextPropagator;
+use OpenTelemetry\API\Trace\TracerInterface;
+use OpenTelemetry\Context\Propagation\TextMapPropagatorInterface;
+use OpenTelemetry\Contrib\Otlp\OtlpHttpTransportFactory;
+use OpenTelemetry\Contrib\Otlp\SpanExporter;
+use OpenTelemetry\SDK\Common\Export\TransportFactoryInterface;
+use OpenTelemetry\SDK\Trace\SpanExporterInterface;
+use OpenTelemetry\SDK\Trace\SpanProcessorFactory;
+use OpenTelemetry\SDK\Trace\SpanProcessorInterface;
+use OpenTelemetry\SDK\Trace\TracerProvider;
+use OpenTelemetry\SDK\Trace\TracerProviderInterface;
+use Psr\Container\ContainerInterface;
+
+/** @var array $params */
+return [
+    TracerInterface::class => static function (ContainerInterface $container) use ($params): TracerInterface {
+        return $container->get(TracerProviderInterface::class)->getTracer(
+            name: $params['ttm/telemetry-otel']['service_name']
+        );
+    },
+    TracerProviderInterface::class => static function (ContainerInterface $container): TracerProviderInterface {
+        return new TracerProvider($container->get(SpanProcessorInterface::class));
+    },
+    SpanExporterInterface::class => static function (ContainerInterface $container) {
+        return new SpanExporter($container->get(TransportFactoryInterface::class));
+    },
+    TransportFactoryInterface::class => static function () use ($params) {
+        return (new OtlpHttpTransportFactory())
+            ->create(
+                endpoint: $params['ttm/telemetry-otel']['endpoint'],
+                contentType: $params['ttm/telemetry-otel']['protocol']
+            );
+    },
+    TextMapPropagatorInterface::class => static function () {
+        return TraceContextPropagator::getInstance();
+    },
+    SpanProcessorInterface::class => static function (ContainerInterface $container) {
+        return (new SpanProcessorFactory())->create($container->get(SpanExporterInterface::class));
+    }
+];
--- config/params.php
+++ config/params.php
@@ -2,4 +2,21 @@
 
 declare(strict_types=1);
 
-return [];
+use TTM\Telemetry\Otel\SystemClock;
+use TTM\Telemetry\Otel\TracerFactory;
+
+return [
+    'ttm/telemetry' => [
+        'drivers' => [
+            'otel' => TracerFactory::class,
+        ],
+        'dependencies' => [
+            'clock' => SystemClock::class
+        ]
+    ],
+    'ttm/telemetry-otel' => [
+        'service_name' => 'Yii Framework',
+        'endpoint' => $_ENV['OTEL_EXPORTER_OTLP_ENDPOINT'] ?? 'http://collector:4318/v1/traces',
+        'protocol' => $_ENV['OTEL_EXPORTER_OTLP_PROTOCOL'] ?? 'application/json'
+    ]
+];
