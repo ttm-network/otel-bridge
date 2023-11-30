d8f025f4d3912dea41e0cab9f491bd3cbf73e8d9
thenotsoft <thenotsoft@gmail.com>
2023-11-30 18:08:05 +0200

    wip

--- config/events-web.php
+++ config/events-web.php
@@ -10,9 +10,11 @@ use Yiisoft\Yii\Http\Event\BeforeRequest;
 return [
     BeforeRequest::class => [
         static function (BeforeRequest $event, ContainerInterface $container): void {
-            $container->get(Context::class)->setContext(
-                $container->get(TextMapPropagatorInterface::class)->extract($event->getRequest()->getHeaders())
-            );
+            $propagator = $container->get(TextMapPropagatorInterface::class);
+            $context = $propagator->extract($event->getRequest()->getHeaders());
+            $carrier = [];
+            $propagator->inject($carrier, context: $context);
+            $container->get(Context::class)->setContext($carrier);
         }
     ]
 ];
