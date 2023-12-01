c96ebd43d1b05b9919d44dee7020d974215c3070
thenotsoft <thenotsoft@gmail.com>
2023-12-01 10:49:10 +0200

    wip

---
+++ src/ContextExtractor.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TTM\Telemetry\Otel;
+
+use OpenTelemetry\Context\Propagation\TextMapPropagatorInterface;
+use TTM\Telemetry\Context;
+use TTM\Telemetry\ContextExtractorInterface;
+
+final class ContextExtractor implements ContextExtractorInterface
+{
+    public function __construct(
+        private readonly TextMapPropagatorInterface $propagator,
+    ) {
+    }
+
+    public function extract(array $data): Context
+    {
+        $context = $this->propagator->extract($data);
+        $carrier = [];
+        $this->propagator->inject($carrier, context: $context);
+
+        return new Context($carrier);
+    }
+}
